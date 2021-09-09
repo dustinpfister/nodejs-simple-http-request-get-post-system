831e64e3fa420d28001d20132eed625f8634be98
dustinpfister <dustin.pfister@gmail.com>
2021-09-09 11:06:33 -0400

    updated index

--- node/static-server-post/index.js
+++ node/static-server-post/index.js
@@ -2,9 +2,11 @@
 /*
  *  index.js
  *
- *   start server to host the root of this project folder
+ *   start server.js of static-server-post with settings for this project folder
  *
- *   ex: $ node index.js 8080
+ *   ex: ( $ node index.js 8080 )
+ *
+ *   which is short for: ( $ node server.js ../../ ../../public 8080 ) from this working folder
  *
  */
 
@@ -12,9 +14,10 @@ let spawn = require('child_process').spawn,
 path = require('path'),
 
 uri_root = path.join(__dirname, '../../'),
+uri_public = path.join(uri_root, 'public'),
 uri_server = path.join(__dirname, 'server.js')
 
-let ls = spawn('node', [uri_server, uri_root, process.argv[2] || '8080']);
+let ls = spawn('node', [uri_server, uri_root, uri_public, process.argv[2] || 8080]);
 
 ls.stdout.on('data', function(data){
     console.log(data.toString());
--- todo.md
+++ todo.md
@@ -16,6 +16,6 @@
 ## () - 0.0.0 - start the project
 * (done) start the project folder based off of the simple sever project folder
 * (done) I will want to have a public folder at the root folder and have that be the place to store static assets
-* start a static-server-post based off of static-server-promise
+* (done) start a static-server-post based off of static-server-promise
 * static-server-post will take three argumnets for folders one for the public folder, the root folder, and port
 * with static-server-post the public folder will be the root path for static requests
